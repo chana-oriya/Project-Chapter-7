fe6a166bd46e43cea0b095eeb1ae50a7c25ee8bd
chana-oriya <chanafeuchtwanger.hilma.tech>
2024-11-27 15:20:46 +0200

    start addUser

--- database/dataRequests/register.js
+++ database/dataRequests/register.js
@@ -16,13 +16,30 @@ function findUser(username, password, callback){
         `INNER JOIN user_password ` + 
         `WHERE user.username = "${username}" AND user_password.user_id = user.id AND user_password.password = "${password}"`,
         function (err, result) {
-        if (err) throw err;
-        if(!result.length)  callback({success: false})
-        else callback({success: true, info: JSON.stringify(result[0])});
+            if (err) throw err;
+            if(!result.length)  callback({success: false})
+            else callback({success: true, info: JSON.stringify(result[0])});
         });
     })
 }
 
-findUser("Bret", "hildegard.org", console.log);
+//findUser("Bret", "hildegard.org1", console.log);
+
+function addUser(user, callback){
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        con.query(`INSERT INTO user (user.name, user.username, user.email) VALUES ` +
+        `('${user.name}', '${user.username}', '${user.email}')`,
+        function (err, result) {
+            if (err) callback({success: false});
+            // console.log(result);
+            // if(!result)  callback({success: false})
+            else callback({success: true, info: JSON.stringify(result)});
+        });
+    })
+}
+
+addUser({name: "chana", username: "chana_f1", email: "chana@g"}, console.log);
 
 module.exports.findUser = findUser;
