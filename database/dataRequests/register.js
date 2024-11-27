a5920cf75ef660194641be5eb069ad1b078f8c9d
chana-oriya <chanafeuchtwanger.hilma.tech>
2024-11-27 16:04:10 +0200

    working addUser database fixed

--- database/dataRequests/register.js
+++ database/dataRequests/register.js
@@ -14,21 +14,15 @@ con.connect(function(err) {
 
 
 function findUser(username, password, callback){
-    con.connect(function(err) {
+    con.query(`SELECT user.id, user.name, user.username, user.email FROM user ` +
+    `INNER JOIN user_password ` + 
+    `WHERE user.username = "${username}" AND user_password.user_id = user.id AND user_password.password = "${password}"`,
+    function (err, result) {
         if (err) throw err;
-        console.log("Connected!");
-        con.query(`SELECT user.id, user.name, user.username, user.email FROM user ` +
-        `INNER JOIN user_password ` + 
-        `WHERE user.username = "${username}" AND user_password.user_id = user.id AND user_password.password = "${password}"`,
-        function (err, result) {
-            if (err) throw err;
-            if(!result.length)  callback({success: false})
-            else callback({success: true, info: JSON.stringify(result[0])});
-        });
+        if(!result.length)  callback({success: false})
+        else callback({success: true, info: JSON.stringify(result[0])});
     })
 }
-
-//findUser("Bret", "hildegard.org1", console.log);
 
 function addUser(user, callback){
     con.query(`INSERT INTO user (user.name, user.username, user.email) VALUES ` +
@@ -42,19 +36,15 @@ function addUser(user, callback){
                 else {
                     const insertedUser = result[0];
                     con.query(`INSERT INTO user_password (password, user_id) VALUES ('${user.password}', ${result[0].id})`,
-                        (err, result) => {
-                            if (err) callback({success: false});
-                            else callback({success: true, info: JSON.stringify(insertedUser)});
-                        }
-                    );  
+                    (err, result) => {
+                        if (err) callback({success: false});
+                        else callback({success: true, info: JSON.stringify(insertedUser)});
+                    });  
                 }
-            }
-            );
+            });
         }
     });
 }
-
-//addUser({name: "chana", username: "chana_feu", email: "chana@g"}, console.log);
 
 module.exports.findUser = findUser;
 module.exports.addUser = addUser;
